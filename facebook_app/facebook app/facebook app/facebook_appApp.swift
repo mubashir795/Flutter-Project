1cd98bffff723ad012a031be9d39c2666ca4848e
mubashir795 <mubasharbhatti795@gmail.com>
2023-02-27 20:04:16 +0500

    Setup xcode and ios simulator and aslo create signup page screen

---
+++ facebook_app/facebook app/facebook app/facebook_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebook_appApp.swift
+//  facebook app
+//
+//  Created by Mubashar Lateef on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct facebook_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
